132553ba9745ba34e37c00a5a4dff33d4ceea365
Meharaz <maharazbhuiyan3@gmail.com>
2025-02-11 21:28:06 +0600

    all api request complete

--- app/api/videos/[id]/route.js
+++ app/api/videos/[id]/route.js
@@ -3,5 +3,23 @@ import videos from "../../../../data/videos.json";
 export async function GET(_request, { params }) {
   const id = params.id;
   const video = videos.find((v) => v.videoId == id);
+  // if () {
+  //   redirect("http://localhost:3000/api/videos");
+  // }
   return NextResponse.json(video);
+}
+export async function PATCH(request, { params }) {
+  const video = await request.json();
+  const id = params.id;
+  const videoIndex = videos.findIndex((v) => v.videoId == id);
+  videos[videoIndex].title = video.title;
+  videos[videoIndex].description = video.description;
+  return NextResponse.json(videos[videoIndex]);
+}
+export async function DELETE(_request, { params }) {
+  const id = params.id;
+  const videoIndex = videos.findIndex((video) => video.videoId == id);
+  const deletedVideo = videos[videoIndex];
+  videos.splice(deletedVideo, 1);
+  return NextResponse.json(deletedVideo);
 }
